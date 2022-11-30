cec31866e08aeec3e3a41ebfab2bac417a09422b
RobertAkhmetov <70895965+RobertAkhmetov@users.noreply.github.com>
2022-11-30 14:00:38 +0300

    Add files via upload

---
+++ Simple2Threads.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using System.Threading; //Именно это пространство имен поддерживает многопоточность
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(func); //Создаем новый объект потока (Thread)
+
+            myThread.IsBackground = true;
+            myThread.Start(); //запускаем поток
+
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Поток 1 выводит " + i);
+                Thread.Sleep(0);
+            }
+
+            Console.Read(); //Приостановим основной поток
+
+        }
+
+        //Функция запускаемая из другого потока
+        static void func()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Поток 2 выводит " + i);
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
