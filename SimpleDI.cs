3ffdd634d72cf2614179322b6cc62248c26fbf9a
RobertAkhmetov <70895965+RobertAkhmetov@users.noreply.github.com>
2022-12-02 10:40:14 +0300

    Add files via upload

---
+++ SimpleDI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+
+    interface ILogger
+    {
+        void Log(string message);
+    }
+    class Logger : ILogger
+    {
+        public void Log(string message) => Console.WriteLine(message);
+    }
+    class Message
+    {
+        ILogger logger;
+        public string Text { get; set; } = "";
+        public Message(ILogger logger)
+        {
+            this.logger = logger;
+        }
+        public void Print() => logger.Log(Text);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message mes = new Message(new Logger());
+            mes.Text = "Hello World!";
+            mes.Print();
+            Console.Read();
+        }
+    }
+}
