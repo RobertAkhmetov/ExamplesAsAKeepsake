4467bf4be3870370cc4dd11c94d6baf86ce5cf4b
RobertAkhmetov <70895965+RobertAkhmetov@users.noreply.github.com>
2022-12-06 10:37:51 +0300

    Add files via upload

---
+++ DelegateAsMethodParam.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoOperation(5, 4, Add);         // 9
+            DoOperation(5, 4, Subtract);    // 1
+            DoOperation(5, 4, Multiply);    // 20
+            Console.Read();
+        }
+
+        static void DoOperation(int a, int b, Operation op)
+        {
+            Console.WriteLine(op(a, b));
+        }
+        static int Add(int x, int y) => x + y;
+        static int Subtract(int x, int y) => x - y;
+        static int Multiply(int x, int y) => x * y;
+
+        delegate int Operation(int x, int y);
+    }
+}
