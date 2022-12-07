e718231177038601adda1d36ed7e0ec7b9832a87
RobertAkhmetov <70895965+RobertAkhmetov@users.noreply.github.com>
2022-12-07 10:00:34 +0300

    Add files via upload

---
+++ SimpleEvent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        class EnteredLetters
+        {
+            public delegate void LettersHandler(string message);
+            public event LettersHandler Notify;
+            public string enteredResult = "abc";
+
+            public EnteredLetters()
+            {
+                Notify += showEvent1;
+                Notify += delegate     //добавим так же анонимный делегат
+                {
+                    Console.WriteLine("анонимный делегат");
+                };
+            }
+            public void AddLetter(string newLet)
+            {
+                enteredResult += newLet;
+                if (enteredResult == "abcd")
+                    Notify?.Invoke("теперь это abcd");
+            }
+
+            public void showEvent1(string message) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"сообщение по событию 1 {enteredResult}: {message}");
+                Console.ResetColor();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            EnteredLetters entLet = new EnteredLetters();
+
+            if (Console.ReadLine() == "d")
+                entLet.AddLetter("d");
+            Console.Read();
+        }
+    }
+}
