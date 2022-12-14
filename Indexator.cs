2e7955182a1b71fc974ec931532e9e0f7f099476
RobertAkhmetov <70895965+RobertAkhmetov@users.noreply.github.com>
2022-12-14 16:21:35 +0300

    add indexator

---
+++ Indexator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Person
+    {
+        public string Name { get; }
+        public Person(string name) => Name = name;
+    }
+    class Company
+    {
+        Person[] personal;
+        public Company(Person[] people) => personal = people;
+
+        public Person this[int index]                           // ИНДЕКСАТОР (int)
+        {
+            get => personal[index]; //возвращение объекта в индексаторе
+            set => personal[index] = value; //установка значения по индексу
+        }
+
+        public Person this[string strParam]                    // ИНДЕКСАТОР не по порядковому номеру
+        {
+            get
+            {
+                switch (strParam)
+                {
+                    case "special":
+                        return new Person("special");
+                    default:
+                        return new Person("default");
+                }
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Person[] myPersonal = { new Person("Robert"), new Person("John"), new Person("Uri") };
+                Company myCompany = new Company(myPersonal);
+                Console.WriteLine(myCompany[0].Name);
+
+                myCompany[0] = new Person("Henry"); //перезапишем по 0му индексу новую персону
+                Console.WriteLine(myCompany[0].Name);
+
+                Console.WriteLine(myCompany["special"].Name); //воспользуемся специальным, не ИНТовым индексом
+            }
+        }
+    }
+}
