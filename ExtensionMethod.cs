ae99208d2c4627624df4a91d9ba2621cb08a5a0e
RobertAkhmetov <70895965+RobertAkhmetov@users.noreply.github.com>
2022-12-19 10:31:24 +0300

    ExtensionMethod

---
+++ ExtensionMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp8
+{
+    static class StringBuilderCustom
+    {
+        public static int CharIndex(this StringBuilder sb, char c)
+        {
+            for(int i = 0; i< sb.Length; i++)
+            {
+                if (sb[i] == c)
+                    return i;
+            }
+            return -1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("fsdsdf");
+            Console.WriteLine(sb.CharIndex('f'));
+            Console.Read();
+        }
+    }
+}
